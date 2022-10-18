cf1b43974adcc7c31b2d50183c8630608a688e16
Jofre Gavidia <jofre.oliveros@correo.usa.edu.co>
2022-10-17 22:45:02 -0500

    first commit

---
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Anotación para ejecutar una aplicación con Spring Boot
+public class FundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+}
